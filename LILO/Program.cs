ae4d9c0699f47cd87fa239682f3d6b47cd1a2020
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2023-11-29 09:48:45 -0300

    Cadastros Funcionários

---
+++ LILO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LILO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            funcionario();
+
+        }
+
+        private static void funcionario()
+        {
+            Origem.LiloEntities origem = new Origem.LiloEntities();
+
+            Destino.virgemEntities destino = new Destino.virgemEntities();
+
+            int i = 1;
+            int count = origem.Cad_Func.Count();
+
+            foreach (var antFun in origem.Cad_Func.ToList())
+            {
+                Destino.pessoa novoFun = new Destino.pessoa();
+
+                novoFun.ISFUNCIONARIO = true;
+                novoFun.NOMERAZAOSOCIAL = antFun.Nome;
+                novoFun.NOMEFANTASIA = antFun.Cargo;
+                novoFun.TRABALHOCARGO = antFun.Cargo;
+                novoFun.DATAADMISSAO = antFun.DataAdmissao;
+                novoFun.ALTERACAODATA = DateTime.Now;
+                novoFun.ALTERACAOUSUARIO = "IMPORTAÇÃO";
+                novoFun.ATIVO = true;
+                novoFun.CADASTROCOMPLETO = true;
+                novoFun.DESCONTOMAXIMOPERMITIDO = 100;
+                novoFun.CONTRIBUINTE = 0;
+                novoFun.CONSUMIDORFINAL = 1;
+                novoFun.PAIS_ID = 30;
+
+                destino.pessoa.Add(novoFun);
+                destino.SaveChanges();
+                i++;
+
+            }
+        }
+    }
+}
+ 
